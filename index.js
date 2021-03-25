d5ed537aa4ed8eead3cfe06ae9d27f64bd1a0663
Brian Firestone <bfirestone2339@gmail.com>
2021-03-24 20:22:04 -0400

    commit tree fdc55be7d8bd06970c1017de96735d609c1faf9f

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-const reducer = (total, element) => total += element;
-
-const totalBatteries = batteryBatches.reduce(function(total, batches){
-    return batches + total;
-})
+const totalBatteries = batteryBatches.reduce(function(total, currentValue){
+        return total + currentValue;
+});
